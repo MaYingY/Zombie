32822d79237e9b43fd2257c6231a9275f5cb9539
MaYingY <MaYingY@1330515655@qq.com>
2017-06-10 19:32:00 +0800

    buildHeader

---
+++ header/创建头文件/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    bool a;
+    int b;
+    scanf("%d",&b);
+    a = (bool)b;
+    cout <<a;
+    cout << "Hello world!" << endl;
+    return 0;
+}
